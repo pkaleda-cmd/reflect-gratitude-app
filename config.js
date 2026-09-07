0fe9de535d61ab883a30a7859bd46ced04d18774
pkaleda-cmd <pkaleda@gmail.com>
2026-09-07 17:19:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste the public values from Supabase Project Settings > API.
-window.REFLECT_CONFIG = { SUPABASE_URL: "https://wkatpdoguqznomcumczi.supabase.co", SUPABASE_ANON_KEY: "sb_publishable_BMc8Vy0kTu899quvJCmrxA_TNwn2ykH" }
+window.REFLECT_CONFIG = { SUPABASE_URL: "https://wkatpdoguqznomcumczi.supabase.co", SUPABASE_ANON_KEY: "sb_publishable_BMc8Vy0kTu899quvJCmrxA_TNwn2ykH" };
